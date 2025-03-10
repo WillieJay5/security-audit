9dd4e8a182efe6b76ba8be41c4e653b0df25b8cb
WillieJay5 <gage.neumaier@gmail.com>
2025-03-10 17:30:33 -0500

    Update network_scan.sh

--- scripts/network_scan.sh
+++ scripts/network_scan.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 echo "🔍 Running Nmap scan..."
 sudo apt install -y nmap
-nmap -sV -A YOUR_SERVER_IP | tee reports/network-scan.log
-
-echo "🚨 Running OpenVAS vulnerability scan..."
-sudo apt install -y openvas
-sudo gvm-start
-echo "Visit https://localhost:9392 to review OpenVAS scan results."
+nmap -sV --script=vuln YOUR_SERVER_IP | tee reports/network-scan.log
